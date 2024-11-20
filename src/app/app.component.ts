37fb8e95ee012555760c03a8db6c379be055f8a3
Jhean <Jhean.2024@cedinsi.edu.co>
2024-11-19 19:24:13 -0500

    tercer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { GaleriaComponent } from "./component/galeria/galeria.component";
+import { ProductService } from './service/product.service';
+import { ProductCardComponent } from './component/product-card/product-card.component';
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, GaleriaComponent, ProductCardComponent, HttpClientModule],
+  providers: [ProductService, HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tiendadecalzado';
+
+  products: any[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductCardComponent } from './component/product-card/product-card.component';
+import { GaleriaComponent } from './component/galeria/galeria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : 'header' , component: HeaderComponent},
+    {path : 'inicio' , component: InicioComponent},
+    {path : 'footer' , component: FooterComponent},
+    {path : 'product-card', component:ProductCardComponent},
+    {path : 'galeria' , component: GaleriaComponent},
+
+
+]
